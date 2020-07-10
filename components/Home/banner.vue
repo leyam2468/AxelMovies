c518baab9ee5206ec8995a9b44bf9644a1c4185c
leyam2468 <leyamm3@gmail.com>
2020-07-10 14:55:49 +0200

    10/7 2:55

--- components/Home/banner.vue
+++ components/Home/banner.vue
@@ -2,42 +2,77 @@
     <div class="banner">
         <v-carousel
             continuous
-            cycle
             height="400"
             hide-delimiter-background
             show-arrows-on-hover
             delimiter-icon="mdi-minus"
         >
             <v-carousel-item
-                v-for="(item, i) in items"
-                :key="i"
-                :src="item.src"
+                v-for="video in videos"
+                :key="video.id"
+                :src="`https://image.tmdb.org/t/p/w780${video.backdrop_path}`"
                 reverse-transition="fade-transition"
                 transition="fade-transition"
             >
-                <v-sheet color="#1e1e1e59" height="100%" tile>
+                <v-sheet color="rgba(30, 30, 30, 0.5)" height="100%" tile>
                     <v-row class="fill-height" align="center" justify="center">
-                        <v-col
-                            ><div class="display-3">
-                                Slide {{ i + 1 }}
-                            </div></v-col
-                        >
-                        <v-col
-                            ><div class="display-3">
-                                Slide {{ i + 1 }}
-                            </div></v-col
-                        >
-                    </v-row>
-                </v-sheet></v-carousel-item
-            >
+                        <v-col md="4"
+                            ><v-row class="" align="center" justify="center">
+                                <v-img
+                                    :src="`https://image.tmdb.org/t/p/w500${video.poster_path}`"
+                                    aspect-ratio="1"
+                                    class="grey lighten-2 elevation-20"
+                                    max-width="300"
+                                    max-height="300"
+                                >
+                                </v-img> </v-row
+                        ></v-col>
+                        <v-col md="8"
+                            ><div class="pa-10">
+                                <h2
+                                    class="color-one font-weight-black display-3"
+                                >
+                                    {{ video.original_title | shorten(40) }}
+                                </h2>
+                                <v-divider class="py-2"></v-divider>
+                                <p>{{ video.overview | shorten(450) }}</p>
+
+                                <v-rating
+                                    v-model="video.vote_average"
+                                    background-color="red lighten-3"
+                                    color="red"
+                                    length="10"
+                                ></v-rating></div
+                        ></v-col>
+                    </v-row> </v-sheet
+            ></v-carousel-item>
         </v-carousel>
+        <div v-for="video in videos" :key="video.id">
+            {{ video }}<v-divider class="py-2"></v-divider>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    filters: {
+        shorten(v, num) {
+            if (v.length > num) {
+                return v.substring(0, num) + ' .....'
+            } else {
+                return v
+            }
+        },
+    },
+    props: {
+        videos: {
+            type: Array,
+            default: null,
+        },
+    },
     data() {
         return {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
             items: [
                 {
                     src:
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <div class="home">
         <v-container>
             <LastNews />
-            <banner />
+            <banner :videos="videos" />
         </v-container>
     </div>
 </template>
@@ -14,6 +14,21 @@ export default {
     components: {
         LastNews,
         banner,
+    },
+    async asyncData({ $axios, error }) {
+        try {
+            const { data } = await $axios.get(
+                'https://api.themoviedb.org/3/trending/movie/day?api_key=e248b861c3ca5e9cf5bb0113718abaf2'
+            )
+            return {
+                videos: data.results,
+            }
+        } catch (e) {
+            error({
+                statusCode: 503,
+                maessage: 'Cant Get vidos, please try angine',
+            })
+        }
     },
 }
 </script>
